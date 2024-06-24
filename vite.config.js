50f3a148c2819fb70cb98462e4aaf3103cb261de
yogesh616 <ys30371@gmail.com>
2024-06-24 12:50:24 +0530

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +9,11 @@ export default defineConfig({
       usePolling: true,
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true, // Optional: for better debugging in production
+  },
   define: {
     __APP_ENV__: process.env.VITE_VERCEL_ENV,
   }
-})
+});
