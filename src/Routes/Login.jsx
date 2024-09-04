b9347f5c356afbd3e456d5886d23e25c2b328f6d
yogesh616 <ys30371@gmail.com>
2024-09-04 22:53:02 +0530

    1.0.2

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -77,17 +77,13 @@ async function login() {
          <p>We are happy to have you back.</p>
          </div> 
          <div className='formPage py-3'>
-<<<<<<< HEAD
-           <input style={{opacity: '1'}} type="text" className="my-3" placeholder="Try test@gmail.com for testing" value={email} onChange={(e)=> setEmail(e.target.value)} aria-describedby="addon-wrapping" />
-           <input style={{opacity: '1'}} type="password" className="my-3" placeholder="testuser" value={password} onChange={(e)=> setPassword(e.target.value)} aria-describedby="addon-wrapping" />
-          <button className='btn btn-primary w-100' style={{background: '#59DCE1'}} onClick={login}>Tester Login</button>
-           <button type="button" onClick={signinwithgoogle}  className="btn w-100 text-dark my-3 gglBtn" style={{background: '#dddfe0'}}><img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="" /> Sign in with Google</button>
-=======
+
+           
            <input type="text" className="my-3" placeholder="Try test@gmail.com for testing" value={email} onChange={(e)=> setEmail(e.target.value)} aria-describedby="addon-wrapping" />
            <input type="password" className="my-3" placeholder="testuser" value={password} onChange={(e)=> setPassword(e.target.value)} aria-describedby="addon-wrapping" />
           <button className='btn btn-primary w-100' style={{background: '#59DCE1'}} onClick={login}>Tester Login</button>
-           <button type="button" onClick={signinwithgoogle}  className="btn w-100 text-dark my-3" style={{background: '#dddfe0'}}><img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="" /> Signin with Google</button>
->>>>>>> 0379fced89ee2b0f8cfb43bdba22eb59b4f455bd
+           <button type="button" onClick={signinwithgoogle}  className="btn w-100 text-dark my-3" style={{background: '#dddfe0'}}><i className="fa-brands fa-google"></i> Signin with Google</button>
+
          </div>
          <div className='bottomText '>Don't have an account <Link to='/signup' style={LinkStyle}>Signup</Link></div>
       </div>
